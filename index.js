429986823981b2407ac1f3c5af535f4c1246de58
Hammad Zafar <2019ee328@student.uet.edu.pk>
2022-12-10 12:31:18 +0500

    helo world

--- index.js
+++ index.js
@@ -136,16 +136,16 @@ app.get("/data", auth, async (req, res) => {
 
 // stories an user 
 app.get("/", async (req, res) => {
-
-  const person = await Person.find({}, { email: 1 })
+  res.send("hello")
+  // const person = await Person.find({}, { email: 1 })
   //const user= await Person.find({},{name:1}).populate('Posts')
 
-  const posts = await Story.find().sort({ _id: -1 })
+  // const posts = await Story.find().sort({ _id: -1 })
   //const user=await Story.find({}).populate('creater')
   //console.log(user.creater[0].name)
-  res.send(
-    posts)
-  res.end()
+  // res.send(
+  //   posts)
+  // res.end()
   // await Story.find({}, (err, result) => {
   //   if (err) {
   // res.json("error")
