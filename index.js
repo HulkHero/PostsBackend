39a64d52d47356a8dc1b65521387d6aac27fe19f
Hammad Zafar <2019ee328@student.uet.edu.pk>
2023-06-09 00:15:25 +0500

    hi

--- index.js
+++ index.js
@@ -47,13 +47,13 @@ app.use((req, res, next) => {
 });
 
 
-app.get('/myPosts/:id', auth, async (rek, res) => {
+app.get('/myPosts/:id', async (rek, res) => {
   try {
     id = rek.params.id
     console.log("id: ", id)
     if (id) {
       const user = await Story.find({ creater: id })
-      res.status(202).send(user)
+      res.send(user)
     }
   }
   catch (error) {
