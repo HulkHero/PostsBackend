af7b21511e1ddcdb5a226c159465ea539dbf7dbf
Hammad Zafar <2019ee328@student.uet.edu.pk>
2023-07-01 14:47:37 +0500

    hi

--- index.js
+++ index.js
@@ -29,10 +29,7 @@ app.use(express.urlencoded({
 }));
 //app.use(bodyParser.json());
 
-app.use(cors({
-  origin: ["http://localhost:3000", "https://hulkposts.netlify.app/"],
-
-}));
+app.use(cors());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
